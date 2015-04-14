aaf851ca33b1c331217a4b851d8b27b7fe3b714c
Nick Wolf <njwolf64@gmail.com>
2015-04-13 20:00:24 -0400

    Add partial views

---
+++ University of Louisville/Vaccines and Travel Clinic/Controllers/PlaceOrderController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vaccines_and_Travel_Clinic.DAL;
+using Vaccines_and_Travel_Clinic.Models;
+
+namespace Vaccines_and_Travel_Clinic.Controllers
+{
+    public class PlaceOrderController : Controller
+    {
+        private ClinicContext db = new ClinicContext();
+        // GET: PlaceOrder
+        public ActionResult Complete()
+        {
+            return View("Complete");
+        }
+
+        public ActionResult Create([Bind(Include = "ID,Name,Description,Count,Price")] Item item)
+        {
+
+
+            if (ModelState.IsValid)
+            {
+                db.Items.Add(item);
+                db.SaveChanges();
+                return RedirectToAction("Complete");
+            }
+
+            return View("_ItemPartialView");
+        }
+    }
+}
